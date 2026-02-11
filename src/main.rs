9d4c17c6dda89ebad3dd573504672d1af7d5e3da
Magomed Alimkhanov <scrythepvp@gmail.com>
2026-02-11 15:11:51 +0100

    feat: working snake game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::io;
 
-use rand::RngExt;
+use rand::{RngExt, rngs::ThreadRng};
 
 const SNAKE_GRID_SIZE: usize = 4;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 struct Position {
     x: usize,
     y: usize,
@@ -15,57 +15,100 @@ impl Position {
     }
 }
 
-fn gen_rand_apple(snake_pos: &Position) -> Position {
-    let mut rng = rand::rng();
-    loop {
-        let x = rng.random_range(0..SNAKE_GRID_SIZE);
-        let y = rng.random_range(0..SNAKE_GRID_SIZE);
-        if snake_pos.x != x && snake_pos.y != y {
-            return Position::new(x, y);
+fn gen_rand_apple(
+    rng: &mut ThreadRng,
+    snake_grid: [[usize; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE],
+    snake_length: usize,
+) -> Position {
+    let len_available_cells = SNAKE_GRID_SIZE.pow(2) - snake_length;
+    let random_apple_i = rng.random_range(0..len_available_cells);
+    let mut i = 0;
+    for (y, snake_grid_row) in snake_grid.iter().enumerate().take(SNAKE_GRID_SIZE) {
+        for (x, snake_grid_cell) in snake_grid_row.iter().enumerate().take(SNAKE_GRID_SIZE) {
+            if *snake_grid_cell != 0 {
+                continue;
+            }
+            if i == random_apple_i {
+                return Position::new(x, y);
+            } else {
+                i += 1;
+            }
         }
     }
+    panic!("no free cell for apple, code bug");
 }
 
-// enum Direction {
-//    W(String::from("w"))
-// }
-
 fn main() {
+    let mut rng = rand::rng();
     let mut snake_grid = [[0; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE];
-    let mut snake_pos = Position::new(0, 0);
-    let mut rand_apple_pos = gen_rand_apple(&snake_pos);
+    // let mut snake_cells = Vec::with_capacity(SNAKE_GRID_SIZE.pow(2));
+    // snake_cells.push(Position::new(0, 0));
+    let mut snake_cells = [Position::new(0, 0); SNAKE_GRID_SIZE.pow(2)];
+    let mut snake_length = 1;
+    let mut gen_apple = false;
+    let mut rand_apple = gen_rand_apple(&mut rng, snake_grid, snake_length);
+    snake_grid[rand_apple.y][rand_apple.x] = 1;
 
-    snake_grid[snake_pos.y][snake_pos.x] = 2;
-    snake_grid[rand_apple_pos.y][rand_apple_pos.x] = 1;
-
-    println!("\x1Bc");
-    for snake_row in snake_grid {
-        println!("{:?}", snake_row);
-    }
-    loop {
+    'game: loop {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let (new_x, new_y) = match input.trim() {
-            "w" => (snake_pos.x, snake_pos.y.saturating_sub(1)),
-            "a" => (snake_pos.x.saturating_sub(1), snake_pos.y),
-            "s" => (snake_pos.x, (snake_pos.y + 1).min(SNAKE_GRID_SIZE - 1)),
-            "d" => ((snake_pos.x + 1).min(SNAKE_GRID_SIZE - 1), snake_pos.y),
-            _ => (snake_pos.x, snake_pos.y),
+        let (dir_x, dir_y): (isize, isize) = match input.trim() {
+            "w" => (0, -1),
+            "a" => (-1, 0),
+            "s" => (0, 1),
+            "d" => (1, 0),
+            _ => (0, 0),
         };
-        let snake_eats_apple = new_x == rand_apple_pos.x && new_y == rand_apple_pos.y;
-        if snake_eats_apple {
-            snake_pos.x = new_x;
-            snake_pos.y = new_y;
-            rand_apple_pos = gen_rand_apple(&snake_pos);
-            snake_grid[rand_apple_pos.y][rand_apple_pos.x] = 1;
-        } else {
-            snake_grid[snake_pos.y][snake_pos.x] = 0;
-            snake_pos.x = new_x;
-            snake_pos.y = new_y;
-        }
-        snake_grid[snake_pos.y][snake_pos.x] = 2;
 
+        let snake_head = snake_cells[0];
+
+        let new_head_x = snake_head.x as isize + dir_x;
+        let new_head_y = snake_head.y as isize + dir_y;
+
+        let new_head_x: usize = new_head_x.clamp(0, SNAKE_GRID_SIZE as isize - 1) as usize;
+        let new_head_y: usize = new_head_y.clamp(0, SNAKE_GRID_SIZE as isize - 1) as usize;
+
+        if snake_head.x == new_head_x && snake_head.y == new_head_y {
+            continue;
+        }
+
+        for snake_cell in snake_cells.iter().take(snake_length - 1).skip(1) {
+            if snake_cell.x == new_head_x && snake_cell.y == new_head_y {
+                continue 'game;
+            }
+        }
+
+        if new_head_x == rand_apple.x && new_head_y == rand_apple.y {
+            snake_length += 1;
+            if snake_length == SNAKE_GRID_SIZE.pow(2) {
+                println!("Game won");
+                return;
+            }
+            gen_apple = true;
+        }
+
+        let snake_tail = snake_cells[snake_length - 1];
+        snake_grid[snake_tail.y][snake_tail.x] = 0;
+        for snake_i in (1..snake_length).rev() {
+            let prev_snake_cell = snake_cells[snake_i - 1];
+            dbg!("{:?}", prev_snake_cell);
+            snake_grid[prev_snake_cell.y][prev_snake_cell.x] = snake_i + 18;
+            snake_cells[snake_i] = prev_snake_cell;
+        }
+
+        let snake_head = &mut snake_cells[0];
+        snake_head.x = new_head_x;
+        snake_head.y = new_head_y;
+        snake_grid[snake_head.y][snake_head.x] = 2;
+
+        if gen_apple {
+            rand_apple = gen_rand_apple(&mut rng, snake_grid, snake_length);
+            snake_grid[rand_apple.y][rand_apple.x] = 1;
+            gen_apple = false;
+        }
+
+        dbg!(snake_length);
         println!("\x1Bc");
         for snake_row in snake_grid {
             println!("{:?}", snake_row);
