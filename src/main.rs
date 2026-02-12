a7a609561ce099cdfb28502faec9349690b8ac55
Magomed Alimkhanov <scrythepvp@gmail.com>
2026-02-12 22:40:48 +0100

    feat: add code to train ai etc.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,61 @@
 use radiate::*;
 
+// Copied from radiate math.rs file
+// use radiate::ops::math;
+// const MAX_VALUE: f32 = 1e+10_f32;
+// const MIN_VALUE: f32 = -1e+10_f32;
+// const ZERO: f32 = 0.0_f32;
+// const fn clamp(value: f32) -> f32 {
+//     if value.is_nan() {
+//         return ZERO;
+//     }
+//
+//     value.clamp(MIN_VALUE, MAX_VALUE)
+// }
+//
+// fn softmax(inputs: &[f32]) -> Vec<f32> {
+//     let max_value = inputs.iter().cloned().reduce(f32::max).unwrap_or(0.0);
+//     let max_value = max_value.clamp(MIN_VALUE, MAX_VALUE);
+//     let exp_values: Vec<f32> = inputs
+//         .iter()
+//         .map(|value| (value - max_value).exp().clamp(MIN_VALUE, MAX_VALUE))
+//         .collect();
+//     let sum_exp_values = exp_values.iter().sum::<f32>();
+//     exp_values
+//         .iter()
+//         .map(|value| clamp(value / sum_exp_values))
+//         .collect()
+// }
+
 mod snake;
 fn main() {
-    let snake_game = snake::Game::new();
-    let score = snake_game.run_game_human();
-    println!("{}", score);
+    let store = vec![
+        (
+            NodeType::Input,
+            vec![Op::var(0), Op::var(1), Op::var(2), Op::var(1)],
+        ),
+        (NodeType::Edge, vec![Op::weight(), Op::identity()]),
+        (NodeType::Vertex, ops::all_ops()),
+        (NodeType::Output, vec![Op::linear()]),
+    ];
+    let codec = GraphCodec::directed(5, 4, store);
+    let engine = GeneticEngine::builder()
+        .codec(codec)
+        .fitness_fn(snake::snake_game_fitness_func)
+        .alter(alters!(
+            GraphMutator::new(0.15, 0.05).allow_recurrent(false),
+            GraphCrossover::new(0.5, 0.5),
+            OperationMutator::new(0.15, 0.15)
+        ))
+        .build();
 
-    // let store = vec![
-    //     (
-    //         NodeType::Input,
-    //         vec![Op::var(0), Op::var(1), Op::var(2), Op::var(1)],
-    //     ),
-    //     (NodeType::Edge, vec![Op::weight(), Op::identity()]),
-    //     (NodeType::Vertex, ops::all_ops()),
-    //     (NodeType::Output, vec![Op::sigmoid()]),
-    // ];
-    // let codec = GraphCodec::directed(5, 4, store);
-    // let engine = GeneticEngine::builder()
-    //     .codec(codec)
-    //     .fitness_fn(snake::snake_game_fitness_func)
-    //     .build();
-    //
-    // let best = engine.iter().take(500).last();
-    // // let genotype = best.unwrap().value().clone();
-    //
+    // let best = radiate::ui(engine).iter().take(50000).last().unwrap();
+    let best = engine.iter().logging().take(10000).last().unwrap();
+    let genotype = best.value().clone();
+
     // println!("\x1Bc");
-    //
-    // println!("{:?}", best.unwrap().score());
-    // snake::snake_game_fitness_func(genotype);
-    //
-    //
+
+    println!("{:?}", best.score());
+    let snake_game = snake::Game::new().print();
+    snake_game.run_game_ai(genotype);
 }
--- src/snake.rs
+++ src/snake.rs
@@ -3,7 +3,13 @@ use std::io;
 use radiate::{Eval, Graph, Op, genotype};
 use rand::{RngExt, rngs::ThreadRng};
 
-const SNAKE_GRID_SIZE: usize = 4;
+const SNAKE_GRID_SIZE: usize = 8;
+const STEPS_TILL_APPLE: i32 = 50;
+const GETTING_APPLE_BONUS: i32 = 1000;
+const STEP_TOWARDS_APPLE_BONUS: i32 = 10;
+const STEP_AWAY_APPLE_BONUS: i32 = -15;
+const WINNING_BONUS: i32 = 50;
+const LOOSING_BONUS: i32 = -1000;
 
 #[derive(PartialEq, Clone, Copy, Debug)]
 struct Position {
@@ -18,7 +24,7 @@ impl Position {
 
 fn gen_rand_apple(
     rng: &mut ThreadRng,
-    snake_grid: [[usize; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE],
+    snake_grid: [[f32; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE],
     snake_length: usize,
 ) -> Position {
     let len_available_cells = SNAKE_GRID_SIZE.pow(2) - snake_length;
@@ -26,7 +32,7 @@ fn gen_rand_apple(
     let mut i = 0;
     for (y, snake_grid_row) in snake_grid.iter().enumerate().take(SNAKE_GRID_SIZE) {
         for (x, snake_grid_cell) in snake_grid_row.iter().enumerate().take(SNAKE_GRID_SIZE) {
-            if *snake_grid_cell != 0 {
+            if *snake_grid_cell != 0.0 {
                 continue;
             }
             if i == random_apple_i {
@@ -45,26 +51,34 @@ enum GameState {
 
 pub struct Game {
     rng: ThreadRng,
-    snake_grid: [[usize; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE],
+    snake_grid: [[f32; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE],
     snake_body_cells: [Position; SNAKE_GRID_SIZE.pow(2)],
     snake_length: usize,
     should_gen_apple: bool,
     rand_apple_pos: Position,
     remaining_steps: i32,
+    steps_away_apple: i32,
     score: i32,
+    should_print: bool,
 }
 
 impl Game {
     pub fn new() -> Game {
         let mut rng = rand::rng();
-        let mut snake_grid = [[0; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE];
-        let snake_body_cells = [Position::new(0, 0); SNAKE_GRID_SIZE.pow(2)];
+        let mut snake_grid = [[0.0; SNAKE_GRID_SIZE]; SNAKE_GRID_SIZE];
+        let rand_snake_head_pos = Position::new(
+            rng.random_range(0..SNAKE_GRID_SIZE),
+            rng.random_range(0..SNAKE_GRID_SIZE),
+        );
+        let snake_body_cells = [rand_snake_head_pos; SNAKE_GRID_SIZE.pow(2)];
         let snake_length = 1;
         let should_gen_apple = false;
         let rand_apple_pos = gen_rand_apple(&mut rng, snake_grid, snake_length);
-        snake_grid[rand_apple_pos.y][rand_apple_pos.x] = 1;
-        let remaining_steps = 15;
+        snake_grid[rand_apple_pos.y][rand_apple_pos.x] = 1.0;
+        let remaining_steps = STEPS_TILL_APPLE;
+        let steps_away_apple = 0;
         let score = 0;
+        let should_print = false;
 
         Game {
             rng,
@@ -74,13 +88,20 @@ impl Game {
             should_gen_apple,
             rand_apple_pos,
             remaining_steps,
+            steps_away_apple,
             score,
+            should_print,
         }
+    }
+
+    pub fn print(mut self) -> Self {
+        self.should_print = true;
+        self
     }
 
     fn take_step(&mut self, dir_x: isize, dir_y: isize) -> GameState {
         if self.remaining_steps <= 0 {
-            self.score -= 50;
+            // self.score += LOOSING_BONUS;
             return GameState::Finished;
         }
         self.remaining_steps -= 1;
@@ -90,8 +111,17 @@ impl Game {
         let new_head_x = snake_head.x as isize + dir_x;
         let new_head_y = snake_head.y as isize + dir_y;
 
-        let new_head_x: usize = new_head_x.clamp(0, SNAKE_GRID_SIZE as isize - 1) as usize;
-        let new_head_y: usize = new_head_y.clamp(0, SNAKE_GRID_SIZE as isize - 1) as usize;
+        if new_head_x < 0 || new_head_x >= SNAKE_GRID_SIZE as isize {
+            self.score += LOOSING_BONUS;
+            return GameState::Finished;
+        }
+        if new_head_y < 0 || new_head_y >= SNAKE_GRID_SIZE as isize {
+            self.score += LOOSING_BONUS;
+            return GameState::Finished;
+        }
+
+        let new_head_x = new_head_x as usize;
+        let new_head_y = new_head_y as usize;
 
         if snake_head.x == new_head_x && snake_head.y == new_head_y {
             return GameState::Continue;
@@ -108,33 +138,49 @@ impl Game {
             }
         }
 
+        let is_new_x_closer = self.rand_apple_pos.x.abs_diff(new_head_x)
+            < self.rand_apple_pos.x.abs_diff(snake_head.x);
+        let is_new_y_closer = self.rand_apple_pos.y.abs_diff(new_head_y)
+            < self.rand_apple_pos.y.abs_diff(snake_head.y);
+        if is_new_x_closer || is_new_y_closer {
+            self.score += STEP_TOWARDS_APPLE_BONUS;
+        } else {
+            self.steps_away_apple += 1
+        }
+
         if new_head_x == self.rand_apple_pos.x && new_head_y == self.rand_apple_pos.y {
             self.snake_length += 1;
+            self.score +=
+                (GETTING_APPLE_BONUS - STEP_AWAY_APPLE_BONUS * self.steps_away_apple).max(0);
+            // if self.should_print {
+            //     dbg!((GETTING_APPLE_BONUS - STEP_AWAY_APPLE_BONUS * self.steps_away_apple).max(0));
+            // }
+            // dbg!(self.score);
             if self.snake_length == SNAKE_GRID_SIZE.pow(2) {
-                self.score += 50;
+                // self.score += WINNING_BONUS;
                 return GameState::Finished;
             }
             self.remaining_steps = 15;
+            self.steps_away_apple = 0;
             self.should_gen_apple = true;
         }
 
         let snake_tail = self.snake_body_cells[self.snake_length - 1];
-        self.snake_grid[snake_tail.y][snake_tail.x] = 0;
+        self.snake_grid[snake_tail.y][snake_tail.x] = 0.0;
         for snake_i in (1..self.snake_length).rev() {
             let prev_snake_cell = self.snake_body_cells[snake_i - 1];
-            // dbg!("{:?}", prev_snake_cell);
-            self.snake_grid[prev_snake_cell.y][prev_snake_cell.x] = snake_i + 18;
+            self.snake_grid[prev_snake_cell.y][prev_snake_cell.x] = snake_i as f32 + 18.0;
             self.snake_body_cells[snake_i] = prev_snake_cell;
         }
 
         let snake_head = &mut self.snake_body_cells[0];
         snake_head.x = new_head_x;
         snake_head.y = new_head_y;
-        self.snake_grid[snake_head.y][snake_head.x] = 2;
+        self.snake_grid[snake_head.y][snake_head.x] = 2.0;
 
         if self.should_gen_apple {
             self.rand_apple_pos = gen_rand_apple(&mut self.rng, self.snake_grid, self.snake_length);
-            self.snake_grid[self.rand_apple_pos.y][self.rand_apple_pos.x] = 1;
+            self.snake_grid[self.rand_apple_pos.y][self.rand_apple_pos.x] = 1.0;
             self.should_gen_apple = false;
         }
         GameState::Continue
@@ -157,7 +203,7 @@ impl Game {
                 return self.score;
             }
 
-            println!("\x1Bc");
+            // println!("\x1Bc");
             for snake_row in self.snake_grid {
                 println!("{:?}", snake_row);
             }
@@ -167,12 +213,8 @@ impl Game {
 
     pub fn run_game_ai(mut self, genotype: Graph<Op<f32>>) -> i32 {
         loop {
-            let observation = self
-                .snake_grid
-                .iter()
-                .flatten()
-                .map(|&x| x as f32)
-                .collect();
+            // let test: Vec<f32> = self.snake_grid.into_iter().flatten().collect();
+            let observation = self.snake_grid.as_flattened().to_vec();
 
             let outputs = genotype.eval(&[observation]);
             let (max_action, _) = outputs[0]
@@ -193,11 +235,14 @@ impl Game {
                 return self.score;
             };
 
-            // println!("\x1Bc");
-            for snake_row in self.snake_grid {
-                println!("{:?}", snake_row);
+            if self.should_print {
+                // dbg!("{}", self.score);
+                // println!("\x1Bc");
+                for snake_row in self.snake_grid {
+                    println!("{:?}", snake_row);
+                }
+                println!("----");
             }
-            println!("----");
         }
     }
 }
